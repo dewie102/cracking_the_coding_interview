ae4dee8248724b90afacc8d49b6c61d4b6a16c6f
Zack Romasz <zromasz@mail.com>
2021-08-06 22:09:09 -0400

    Starting c# project to track code/ progress

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cracking_the_coding_interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
